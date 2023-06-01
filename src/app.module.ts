ed4c2356c1d86011b22599b727badbb1c39768c5
Your Name <you@example.com>
2023-06-01 15:16:41 +0700

    OK

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { ProductModule } from './product/product.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: '',
+      password: 'root',
       database: 'pos_db',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
