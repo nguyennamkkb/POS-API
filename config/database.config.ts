4fae157044440048a4be20e5fd92a317f1e6b41a
Your Name <you@example.com>
2023-06-02 13:04:31 +0700

    wwww

---
+++ config/database.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'pos_db',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+};
